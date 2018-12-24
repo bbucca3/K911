d68bdcfd1d885fb36f54b9a99248ae965b02dc7a
Ben Bucca <benjamin.bucca3@gmail.com>
2018-12-24 15:04:41 -0500

    Updates Shelter

--- K911/Objects/Shelter.swift
+++ K911/Objects/Shelter.swift
@@ -10,9 +10,23 @@ import Foundation
 import SwiftyJSON
 
 struct Shelter {
-    let name: String
+    let name: String?
+    let id: String?
+    let email: String?
+    let phone: String?
+    let city: String?
+    let country: String?
+    let state: String?
+    let address: String?
     
     init(json: JSON) {
-        self.name = json["placeholder"].stringValue
+        self.name = json["name"]["$t"].stringValue
+        self.id = json["id"]["$t"].stringValue
+        self.email = json["email"]["$t"].stringValue
+        self.phone = json["phone"]["$t"].stringValue
+        self.city = json["city"]["$t"].stringValue
+        self.country = json["country"]["$t"].stringValue
+        self.state = json["state"]["$t"].stringValue
+        self.address = json["address1"]["$t"].stringValue
     }
 }
