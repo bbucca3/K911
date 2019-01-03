58dde20e465da4c7ef2d3114bf3036755a39910b
Ben Bucca <benjamin.bucca3@gmail.com>
2019-01-02 20:31:33 -0500

    Creates SheltersViewCell

---
+++ K911/Models/SheltersViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SheltersViewCell.swift
+//  K911
+//
+//  Created by Benjamin Bucca on 1/2/19.
+//  Copyright © 2019 Benjamin Bucca. All rights reserved.
+//
+
+import UIKit
+
+class SheltersViewCell: UITableViewCell {
+
+    
+
+}
