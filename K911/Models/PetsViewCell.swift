b78219677e008afc50a2dccaa227719bdb5f4592
Ben Bucca <benjamin.bucca3@gmail.com>
2019-01-04 21:43:01 -0500

    Creates PetsViewCell

---
+++ K911/Models/PetsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PetsViewCell.swift
+//  K911
+//
+//  Created by Benjamin Bucca on 1/4/19.
+//  Copyright © 2019 Benjamin Bucca. All rights reserved.
+//
+
+import UIKit
+
+class PetsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
